9019ac402e39cca5dc6197fa165285da97c35e8a
Hanhusaso <hans.e.huiza.n@gmail.com>
2023-04-13 08:08:38 -0500

    feat: add testimonios

--- components/home/Testimonios.js
+++ components/home/Testimonios.js
@@ -27,12 +27,10 @@ const Testimonios = () => {
 			initial="offscreen"
 			whileInView="onscreen"
 			viewport={{ once: true, amount: 0.3 }}
-			className="bg-blanco mb-14 md:mb-18 animate__animated animate__fadeInUp"
-		>
+			className="bg-blanco mb-14 md:mb-18 animate__animated animate__fadeInUp">
 			<motion.div
 				variants={entradaCardScroll}
-				className="container mx-auto px-4 md:grid-cols-12 gap-x-4 md:grid"
-			>
+				className="container mx-auto px-4 md:grid-cols-12 gap-x-4 md:grid">
 				<div className="col-start-2 col-span-10 flex md:mb-6">
 					<h2 className="text-4xl font-bold text-tertiary">
 						Testimonios
@@ -63,8 +61,7 @@ const Testimonios = () => {
 											testimoniosData[swiper.realIndex]
 										);
 									});
-								}}
-							>
+								}}>
 								<SwiperButtonNext />
 								{testimoniosData.map((testimonio) => (
 									<SwiperSlide key={testimonio.nombre}>
@@ -83,10 +80,10 @@ const Testimonios = () => {
 								))}
 							</Swiper>
 						)}
-						<div className="static md:absolute md:w-1/2 md:left-1/2 md:top-1/2 md:-translate-y-1/2">
+						<div className="static md:absolute md:w-1/2 md:left-1/2 md:top-1/2 md:-translate-y-1/2 rounded-lg overflow-hidden">
 							<div className="relative">
-								<div className="bg-complementaryTwo px-6 md:pl-[32%] py-8 md:pr-11 rounded-lg">
-									<div className="rounded-lg bg-tertiary w-16 h-1 mb-3"></div>
+								<div className="bg-complementaryTwo px-6 md:pl-[32%] pt-8 pb-9 md:pr-11">
+									{/* <div className="rounded-lg bg-tertiary w-16 h-1 mb-3"></div> */}
 									<p className="text-xl text-tertiary font-bold mb-2">
 										{testimonioElegido.nombre}
 									</p>
@@ -94,7 +91,9 @@ const Testimonios = () => {
 										{testimonioElegido.frase}
 									</p>
 								</div>
-								<div className="hidden md:block absolute right-0 bottom-0 bg-secondary w-[4.271rem] h-[4.271rem] rounded-full translate-x-1/2 translate-y-1/2 -z-10"></div>
+								{/* <div className="hidden md:block absolute right-0 bottom-0 bg-secondary w-[4.271rem] h-[4.271rem] rounded-full translate-x-1/2 translate-y-1/2 -z-10"></div> */}
+								<div className="absolute left-0 bottom-0 bg-secondary w-1/3 md:w-1/2 h-[10px]"></div>
+								<div className="absolute right-0 bottom-0 bg-primary w-2/3 md:w-1/2 h-[10px]"></div>
 							</div>
 						</div>
 					</div>
--- data/home/testimonios.js
+++ data/home/testimonios.js
@@ -1,33 +1,18 @@
 const testimoniosData = [
 	{
-		nombre: 'Susana Vanessa Calderón Toledo',
-		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-farmacia-bioquimica/testimonios/susana-vanessa-calderon-testimonio.jpg',
-		frase: '“La Universidad Nacional Mayor de San Marcos me brindó los conocimientos, experiencias y vivencias que contribuyeron en mi formación profesional y personal, una de las mejores épocas de mi vida que no olvidaré.”',
+		nombre: 'Heydy Merly Torres Ramos',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-quimica/testimonios/heydi-torres-ingenieria-agroindustrial.jpg',
+		frase: '“Aprovechen todas las formas de conocimiento y úsenlo para crecer como personas, motivar a otros; háganse ciudadanos del mundo.”',
 	},
 	{
-		nombre: 'Adrián Arturo Intiquilla Quispe',
-		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-farmacia-bioquimica/testimonios/adrian-arturo-intiquilla-quispe.jpg',
-		frase: '“La carrera de Ciencia de los Alimentos es un mundo apasionante que nos permite conocer la complejidad e importancia del alimento desde un punto de vista científico y tecnológico, y lo hace aún más interesante realizarlo en la Decana de América. A todos los futuros estudiantes recordar siempre que el esfuerzo de hoy es el éxito del mañana.”',
+		nombre: 'Kevin Arroyo Vilela',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-quimica/testimonios/kevin-arroyo-quimica-quimica.jpg',
+		frase: '“A los jóvenes que quieran estudiar esta carrera, yo les recomiendo que siempre busquen una manera diferente de hacer las cosas; y a la Universidad Nacional Mayor de San Marcos le digo gracias por dejarme crecer y motivarnos a hacer las cosas cada vez mejor.”',
 	},
 	{
-		nombre: 'Natalia Bullon Zegarra',
-		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-farmacia-bioquimica/testimonios/natalia-bullon-zegarra-testimonio.jpg',
-		frase: '“A veces las cosas parecen difíciles al principio, sin embargo, si uno es comprometido, disciplinado y está dispuesto a sacrificarse, entonces uno se relaciona con lo difícil de una manera diferente. Así, uno se motiva ante las dificultades y las toma como su propio camino de aprendizaje.”',
-	},
-	{
-		nombre: 'Johnny Curo Manchego',
-		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-farmacia-bioquimica/testimonios/jhonny-curo-machengo.jpg',
-		frase: '“La carrera de Ciencia de los Alimentos me dio una visión holística del alimento y me enseñó a entenderlo desde la perspectiva científica e industrial. Estudiar en San Marcos es una experiencia única y emprender esta aventura es sin duda la mejor decisión que tomé.”',
-	},
-	{
-		nombre: 'Carol Nathali Flores Fernández',
-		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-farmacia-bioquimica/testimonios/carol-nathali-flotes-fernandez-testimonio.jpg',
-		frase: '“El camino para lograr las metas no siempre es sencillo ni corto. Es fundamental dar el primer paso y mirar siempre adelante, a pesar de las dificultades que se puedan presentar. El esfuerzo y trabajo constante siempre te llevarán a donde quieres llegar.”',
-	},
-	{
-		nombre: 'Jessica Yvonne Bendezú Ccanto',
-		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-farmacia-bioquimica/testimonios/jessica-yvonne-bendezu-ccanto.jpg',
-		frase: '“Las mujeres podemos llegar tan lejos como lo deseemos, también podemos ser científicas solo es cuestión de esfuerzo y actitud, eliminemos de nuestro vocabulario las killer phrases o frases limitantes como: ‘Esta es una carrera de hombres’ o ‘esta carrera no es para ti porque no eres buena en ciencias’, al contrario, seamos la puerta de entrada para que más jóvenes decidan por esta hermosa profesión.”',
+		nombre: 'Ruth Zintya Vargas Sosa',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fac-quimica/testimonios/ruth-zintya-vargas-sosa-ing-quimica.jpg',
+		frase: '“La carrera de ingeniería química te permite tener una versatilidad única en las industrias, mi consejo para ustedes es tener siempre la mejor actitud, predisposición para aprender y demostrar que eres muy capaz. Las oportunidades siempre aparecerán, solo hay que estar listos e ir por ellas.”',
 	},
 ];
 
