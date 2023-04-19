72aabfe8f01efd6331b4d69ba418e8f34fc06d7a
Hanhusaso <hans.e.huiza.n@gmail.com>
2023-04-19 17:10:31 -0500

    feat: update styles in footer

--- components/shared/FooterComponent.js
+++ components/shared/FooterComponent.js
@@ -25,32 +25,36 @@ const FooterComponent = ({ inHome }) => {
 			</div>
 			<div className="bg-footerOne">
 				<div className="container mx-auto px-3 grid-cols-12 gap-x-4 md:grid block pt-20 pb-6">
-					<div className="md:col-start-2 md:col-span-10 text-blanco grid grid-cols-2 md:grid-cols-[auto_auto_auto_auto] md:grid-rows-1 gap-x-4 md:gap-x-10 gap-y-14 md:gap-y-0 px-14">
-						<div className="col-span-2 md:col-span-1 md:max-w-[225px]">
-							<div className="grid grid-cols-[50px_auto] gap-x-4 gap-y-[18px] w-full mb-3">
+					<div className="md:col-start-2 md:col-span-10 text-blanco grid grid-cols-2 md:grid-cols-[281px_auto_auto_auto_auto] md:grid-rows-1 gap-x-4 md:gap-x-10 gap-y-14 md:gap-y-0 px-14">
+						<div className="col-span-2 md:col-span-1">
+							<div className="grid grid-cols-[84px_auto] gap-x-4 gap-y-[18px] w-full mb-3">
 								<Image
 									src="https://posgrado-unmsm.s3.sa-east-1.amazonaws.com/logo_unmsm_2_8e2e045cd4.png"
 									alt="logo de la UNMSM"
-									width={50}
-									height={61}
+									width={84}
+									height={103}
 								/>
 								<div>
 									<p className="font-bold mb-2">
 										Facultad de Química e Ingeniería Química
 									</p>
-									<p className="font-semibold text-xs">
-										Universidad Nacional Mayor de San Marcos
+									<p className="text-sm">
+										<b>Dirección:</b> Av. Carlos Germán
+										Amezaga #375 - Cercado de Lima (
+										<a
+											href="https://goo.gl/maps/6jXz2gUmn5QKXWdh9"
+											target="_blank"
+											className="font-bold underline"
+											rel="noopener noreferrer">
+											ver en el mapa
+										</a>
+										)
 									</p>
 								</div>
 							</div>
-							<p className="text-xs">
-								Somos una institución pionera y referente en la
-								formación de profesionales farmacéuticos a nivel
-								nacional
-							</p>
 						</div>
 						<div className="">
-							<div className="col-span-1 md:block">
+							{/* <div className="col-span-1 md:block">
 								<p className="mb-6">
 									<Link href="/formacion-academica/admision">
 										<a>Admisión</a>
@@ -66,7 +70,12 @@ const FooterComponent = ({ inHome }) => {
 										<a>Posgrado</a>
 									</Link>
 								</p>
-							</div>
+							</div> */}
+							<p className="mb-6">
+								<b>Teléfono:</b> (01) 6197000
+								<br />
+								Anexo: 1209
+							</p>
 
 							<p className="mb-6">
 								<Link href="/tramites">
@@ -75,30 +84,31 @@ const FooterComponent = ({ inHome }) => {
 							</p>
 						</div>
 						<div className="">
-							{/* <p className="mb-6">
-							<Link href="/">
-								<a>Historia</a>
-							</Link>
-						</p> */}
-							{/* <p className="mb-6">
-							<a
-								href=""
-								className="inline-block"
-								onClick={(e) => {
-									e.preventDefault();
-									window.open(
-										'https://patch-knot-df8.notion.site/FFB-Informaci-n-acad-mica-9a6b64da3bf144b29098948e6c87bd75',
-										'_blank'
-									);
-								}}>
-								Información
-								<br />
-								académica
-							</a>
-						</p> */}
 							<p className="mb-6">
-								<Link href="/nosotros/autoridades-y-organos">
-									<a>Autoridades y órganos de control</a>
+								<Link href="/admision/pregrado">
+									<a>Admisión Pregrado</a>
+								</Link>
+							</p>
+							<p className="mb-6">
+								<Link href="/admision/posgrado">
+									<a>Admisión Posgrado</a>
+								</Link>
+							</p>
+							<p className="">
+								<Link href="//comunicados">
+									<a>Comunicados</a>
+								</Link>
+							</p>
+						</div>
+						<div className="">
+							<p className="mb-6">
+								<Link href="/formacion-academica/pregrado">
+									<a>Pregrado</a>
+								</Link>
+							</p>
+							<p className="mb-6">
+								<Link href="/formacion-academica/posgrado">
+									<a>Posgrado</a>
 								</Link>
 							</p>
 							<p className="">
@@ -108,39 +118,23 @@ const FooterComponent = ({ inHome }) => {
 							</p>
 						</div>
 						<div className="col-span-2 md:col-span-1">
-							<div className="mb-7">
-								<p className="mb-1">
-									<span>
-										<UbicacionStroke className="fill-blanco inline relative -top-[1px] mr-1" />
-									</span>
-									<span className="font-bold">Ubicación</span>
-								</p>
-								<p>Jr. Puno N.° 1002, Lima, Perú</p>
-							</div>
-
-							<div className="mb-7">
-								<p className="mb-1">
-									<span>
-										<TelefonoStroke className="fill-blanco inline relative -top-[1px] mr-1" />
-									</span>
-									<span className="font-bold">Teléfono</span>
-								</p>
-								<p>
-									(+51) 619 7000 (
-									<Link href="/directorio">
-										<a className="font-bold">
-											Ver directorio
-										</a>
-									</Link>
-									)
-								</p>
-							</div>
+							<p className="mb-6">
+								<Link href="/nosotros/autoridades-y-organos">
+									<a>Autoridades</a>
+								</Link>
+							</p>
+							<p className="">
+								<Link href="/directorio">
+									<a>Directorio</a>
+								</Link>
+							</p>
 						</div>
 					</div>
 				</div>
-				<p className="col-start-3 col-span-8 text-center text-blanco mt-10 md:mt-28 py-3 text-xs bg-negroPuro/40">
-					Diseñado, desarrollado e implementado por el Equipo Especial
-					Web de la Oficina General de Imagen Institucional-UNMSM-2022
+				<p className="col-start-3 col-span-8 text-center text-blanco/25 mt-10 md:mt-[88px] py-6 text-xs">
+					Diseñado, desarrollado e implementado por el equipo especial
+					web | Oficina General de Imagen e Institucional de la
+					Universidad Nacional Mayor San Marcos
 				</p>
 			</div>
 		</footer>
