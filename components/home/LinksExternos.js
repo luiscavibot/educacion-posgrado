30938d5e2be47e493caea7826b61beab6377fe86
Hanhusaso <hans.e.huiza.n@gmail.com>
2023-08-07 23:12:44 -0500

    feat: add link externos in home

--- components/home/LinksExternos.js
+++ components/home/LinksExternos.js
@@ -13,6 +13,8 @@ import HojaLupaStroke from '../icons/HojaLupaStroke';
 import ManoCorazon from '../icons/ManoCorazon';
 import LibroCintaStroke2 from '../icons/LibroCintaStroke2';
 import GrupoPersonasTres from '../icons/GrupoPersonasTres';
+import AdmisionHomeIcon from '../icons/AdmisionHomeIcon';
+import BlogGestionPublicaHomeIcon from '../icons/BlogGestionPublicaHomeIcon';
 
 const LinksExternos = () => {
 	return (
@@ -20,131 +22,70 @@ const LinksExternos = () => {
 			initial="offscreen"
 			whileInView="onscreen"
 			viewport={{ once: true, amount: 0.3 }}
-			className="bg-blanco mb-14 md:mb-18 animate__animated animate__fadeInUp">
+			className="bg-blanco mb-14 md:mb-18 animate__animated animate__fadeInUp"
+		>
 			<motion.div
 				variants={entradaCardScroll}
-				className="container mx-auto px-4 md:grid-cols-12 gap-x-4 md:grid">
+				className="container mx-auto px-4 md:grid-cols-12 gap-x-4 md:grid"
+			>
 				<div className="col-start-2 col-span-10">
 					{/* <div className="w-full lg:w-auto lg:max-w-[1022px] md:mx-auto grid grid-cols-2 lg:grid-cols-4 place-items-center gap-x-4 gap-y-8 lg:gap-x-[60px] mb-8"> */}
-					<div className="w-full lg:w-auto lg:max-w-[1022px] md:mx-auto grid grid-cols-2 lg:grid-cols-4 place-items-center gap-x-4 gap-y-8 lg:gap-x-[60px]">
-						<Link href="/formacion-academica/posgrado">
-							<a className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
+					<div className="w-full lg:w-auto lg:max-w-[1022px] md:mx-auto grid grid-cols-2 lg:grid-cols-5 place-items-center gap-x-4 gap-y-8 lg:gap-x-[60px]">
+						<Link href="/admision?programa=maestria">
+							<a className="group bg-secondary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[188px] h-[140px] flex flex-col items-center justify-center px-5 cursor-pointer">
 								<div>
-									<BirreteStroke className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
+									<AdmisionHomeIcon className="fill-blanco group-hover:fill-blanco transition-colors duration-300 h-9" />
 								</div>
-								<p className="text-blanco text-xl">Posgrado</p>
+								<p className="text-blanco text-lg">
+									Admisión de Mestrías
+								</p>
 							</a>
 						</Link>
-						<a
-							onClick={() => {
-								window.open(
-									'https://sisbib.unmsm.edu.pe/?page=2',
-									'_blank'
-								);
-							}}
-							className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
-							<div>
-								<LupaStroke className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
-							</div>
-							<p className="text-blanco text-xl">
-								Recursos de investigación
-							</p>
-						</a>
-						<Link href="/tramites">
-							<a className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
+						<Link href="/investigacion/actividad-cientifica">
+							<a className="group bg-secondary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[188px] h-[140px] flex flex-col items-center justify-center px-5 cursor-pointer">
 								<div>
-									<DocumentClipIcon className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
+									<LupaStroke className="fill-blanco group-hover:fill-blanco transition-colors duration-300 h-9" />
 								</div>
-								<p className="text-blanco text-xl">Trámites</p>
+								<p className="text-blanco text-lg">
+									Actividad científica
+								</p>
 							</a>
 						</Link>
+
 						<a
-							onClick={() => {
-								window.open(
-									'https://sisbib.unmsm.edu.pe/',
-									'_blank'
-								);
-							}}
-							// onClick={(e) => {
-							// 	e.preventDefault();
-							// }}
-							className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
+							href="#"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="group bg-secondary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[188px] h-[140px] flex flex-col items-center justify-center px-5 cursor-pointer"
+						>
 							<div>
-								<EdificioBibliotecaStroke className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
+								<LibroCintaStroke2 className="fill-blanco group-hover:fill-blanco transition-colors duration-300 h-11" />
 							</div>
-							<p className="text-blanco text-xl">
-								Sistemas de bibliotecas
-							</p>
+							<p className="text-blanco text-lg">Publicaciones</p>
 						</a>
+
+						<Link href="/blog-gestion-publica">
+							<a className="group bg-secondary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[188px] h-[140px] flex flex-col items-center justify-center px-5 cursor-pointer">
+								<div>
+									<BlogGestionPublicaHomeIcon className="fill-blanco group-hover:fill-blanco transition-colors duration-300 h-9" />
+								</div>
+								<p className="text-blanco text-lg">
+									Blog de Gestión Pública
+								</p>
+							</a>
+						</Link>
+
+						<Link href="/nosotros/transparencia">
+							<a className="group bg-secondary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[188px] h-[140px] flex flex-col items-center justify-center px-5 cursor-pointer">
+								<div>
+									<HojaLupaStroke className="fill-blanco group-hover:fill-blanco transition-colors duration-300 h-9" />
+								</div>
+								<p className="text-blanco text-lg">
+									Transparencia
+								</p>
+							</a>
+						</Link>
 					</div>
-					{/* <div className="w-full grid grid-cols-2 gap-x-4 gap-y-8 md:flex justify-between">
-						<a
-							onClick={() => {
-								window.open(
-									'https://revistasinvestigacion.unmsm.edu.pe/index.php/repiie',
-									'_blank'
-								);
-							}}
-							// onClick={(e) => {
-							// 	e.preventDefault();
-							// }}
-							className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
-							<div>
-								<ManoCorazon className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
-							</div>
-							<p className="text-blanco text-xl">CERSEU</p>
-						</a>
-						<a
-							// onClick={() => {
-							// 	window.open(
-							// 		'https://quipucamayoc.unmsm.edu.pe/PortalEmpleo/#/login/alumno',
-							// 		'_blank'
-							// 	);
-							// }}
-							onClick={(e) => {
-								e.preventDefault();
-							}}
-							className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
-							<div>
-								<GrupoPersonasTres className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
-							</div>
-							<p className="text-blanco text-xl">Comunidad</p>
-						</a>
-						<a
-							// onClick={() => {
-							// 	window.open(
-							// 		'https://sisbib.unmsm.edu.pe/',
-							// 		'_blank'
-							// 	);
-							// }}
-							onClick={(e) => {
-								e.preventDefault();
-							}}
-							className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
-							<div>
-								<HojaLupaStroke className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
-							</div>
-							<p className="text-blanco text-xl">Transparencia</p>
-						</a>
-						<a
-							// onClick={() => {
-							// 	window.open(
-							// 		'https://quipucamayoc.unmsm.edu.pe/PortalEmpleo/#/login/alumno',
-							// 		'_blank'
-							// 	);
-							// }}
-							onClick={(e) => {
-								e.preventDefault();
-							}}
-							className="group bg-tertiary hover:bg-primary transition-colors duration-300 text-center rounded-lg w-full md:w-[211px] h-[190px] flex flex-col items-center justify-center px-5 cursor-pointer">
-							<div>
-								<LibroCintaStroke2 className="fill-blanco group-hover:fill-blanco transition-colors duration-300 w-14" />
-							</div>
-							<p className="text-blanco text-xl">
-								Educación continua
-							</p>
-						</a>
-					</div> */}
 				</div>
 			</motion.div>
 		</motion.section>
