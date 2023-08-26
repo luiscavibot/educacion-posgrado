b8b3151ca30ba44ee319c6ad5c95f150488a3f31
luiscavibot <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2023-08-25 22:11:17 -0500

    multiples

---
+++ data/testimonios.js
@@ -0,0 +1,43 @@
+const testimoniosData = [
+	{
+		nombre: 'María Carranza Ancajima',
+		programa: 'Maestría en Gestión Pública',
+		tipoPrograma: 'maestrias',
+		cargo: 'Directora de la Oficina de Imagen Institucional en el Ministerio de Vivienda, Construcción y Saneamiento',
+		linkedin:
+			'https://www.linkedin.com/in/marianacarranzaancajima/?originalSubdomain=pe',
+		frase: 'La maestría me entrenó para tener una visión estratégica en la gestión. Desde el análisis y debate sobre los problemas y potencialidades del país hasta la aplicación de herramientas para realizar propuestas coherentes que generen valor.',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fca-posgrado/fotos-egresados/carranza.jpg',
+	},
+	{
+		nombre: 'Alberto Florez Florez',
+		programa: 'Maestría en Marketing',
+		tipoPrograma: 'maestrias',
+		cargo: 'Gerente General de International Freight Shipping SAC',
+		linkedin: 'https://www.linkedin.com/in/alberto-florez-05654352/',
+		frase: 'Estudiar la maestría ha sido una grata experiencia, tanto a nivel académico como personal. El nivel de los profesores ha sido excelente. He podido aprender diferentes teorías, conceptos y temas de actualidad. La expectativa que tenía se ha cumplido.',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fca-posgrado/fotos-egresados/florez.jpg',
+	},
+	{
+		nombre: 'Esther Contreras Galván',
+		programa: 'Maestría en Dirección de Recursos Humanos',
+		tipoPrograma: 'maestrias',
+		cargo: 'Jefe de RRHH y Staffing de Ventura Soluciones',
+		linkedin:
+			'https://www.linkedin.com/in/esther-contreras-galv%C3%A1n-b730a5b6/',
+		frase: 'Invertir en nuestro desarrollo profesional es la mejor decisión. La maestría, a través de su enfoque analítico y multidisciplinario, me ha permitido escalar hacia nuevas oportunidades laborales, afrontando cada reto con mayor seguridad y confianza.',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fca-posgrado/fotos-egresados/contreras.jpg',
+	},
+	{
+		nombre: 'Mauro Sandoval Unchón',
+		programa: 'Maestría en Gestión Empresarial',
+		tipoPrograma: 'maestrias',
+		cargo: 'Gestor de Patrimonio Inmobiliario de Real Estate Inmobiliaria',
+		linkedin:
+			'https://www.linkedin.com/in/mauro-sandoval-unch%C3%B3n-13ba934a/',
+		frase: 'Estudié la maestría en San Marcos por su prestigio y compromiso con la calidad. Además, la estancia en Europa me confirmó lo que nos enseñaron allí: el mundo está altamente integrado en términos industriales y tecnológicos.',
+		foto: 'https://unmsm-web-static-files.s3.sa-east-1.amazonaws.com/fca-posgrado/fotos-egresados/unchon.jpg',
+	},
+];
+
+export default testimoniosData;
