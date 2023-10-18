060ecdf85288bc46193caf43c6f31f32f582f39d
luiscavibot <luisivan.castillovillafuerte@externos-cl.cencosud.com>
2023-10-18 18:29:10 -0500

    feat: add protocolos data

--- data/protocolosInvestigacion.js
+++ data/protocolosInvestigacion.js
@@ -2,28 +2,28 @@ const protocolosInvestigacion = [
 	{
 		n: '1',
 		nombreDocumento:
-			'Estructura del proyecto de tesis de maestría o doctorado de investigación cuantitativa',
+			'Estructura del Proyecto de Tesis de Maestría o Doctorado - Investigación Cuantitativa',
 		LinkDocumento:
 			'https://drive.google.com/file/d/10GEVZr_41b-4cyB9nQLMm3Zwy8iJwG-X/view?usp=sharing',
 	},
 	{
 		n: '2',
 		nombreDocumento:
-			'Estructura del proyecto de tesis de maestría o doctorado investigación cualitativa',
+			'Estructura del Proyecto de Tesis de Maestría o Doctorado - Investigación Cualitativa',
 		LinkDocumento:
 			'https://drive.google.com/file/d/1ZfkC4UTY2yYNglB5mUO_Jbdbiu73iR8D/view?usp=sharing',
 	},
 	{
 		n: '3',
 		nombreDocumento:
-			'Estructura de una tesis de maestría o doctorado investigación cuantitativa',
+			'Estructura de Tesis de Maestría o Doctorado - Investigación Cuantitativa',
 		LinkDocumento:
 			'https://drive.google.com/file/d/1helcLAl5xMsh1OnZX_YVNLJP0wGBGVZ-/view?usp=sharing',
 	},
 	{
 		n: '4',
 		nombreDocumento:
-			'Estructura de una tesis de maestría o doctorado investigación cualitativa',
+			'Estructura de Tesis de Maestría o Doctorado - Investigación Cualitativa',
 		LinkDocumento:
 			'https://drive.google.com/file/d/1Z39OYeyQejWEOMqinYVdYXbDgXOI46vU/view?usp=sharing',
 	},
